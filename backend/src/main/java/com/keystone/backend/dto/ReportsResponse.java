fb15168eb11d55082517336d4f7c2dec2055d2a9
Akshada Kurhade <akshadakurhade8@gmail.com>
2026-07-24 11:51:52 +0530

    removed duplication

--- backend/src/main/java/com/keystone/backend/dto/ReportsResponse.java
+++ backend/src/main/java/com/keystone/backend/dto/ReportsResponse.java
@@ -1,14 +1,5 @@
 package com.keystone.backend.dto;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 
 
 public class ReportsResponse {
